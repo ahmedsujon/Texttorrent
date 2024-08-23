f5b1ac9773e7ec55e321f30551437f1e8f0c1790
nz-hridoy <nayeemuzaman05@gmail.com>
2024-08-23 12:09:17 +0600

    base setup

---
+++ app/Livewire/App/Layouts/Inc/Sidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Layouts\Inc;
+
+use Livewire\Component;
+
+class Sidebar extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.layouts.inc.sidebar');
+    }
+}
