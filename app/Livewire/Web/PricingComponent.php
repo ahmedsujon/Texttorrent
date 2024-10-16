f683623715ad659ee3e783909e63d06e08203213
ahmedsujon <sujonahmed424@gmail.com>
2024-10-16 17:05:04 +0600

    subscription migration

--- app/Livewire/Web/PricingComponent.php
+++ app/Livewire/Web/PricingComponent.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class PricingComponent extends Component
 {
+    public $user_id, $package_type, $package_name, $amount;
+
     public function render()
     {
         return view('livewire.web.pricing-component')->layout('livewire.web.layouts.base');
