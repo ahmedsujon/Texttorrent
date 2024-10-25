c6236c5272ade13f3dadc9f2fad0ac983cad2a16
ahmedsujon <sujonahmed424@gmail.com>
2024-10-25 19:22:45 +0600

    subscription filter

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ChatTableSeeder::class);
         $this->call(InboxTemplateTableSeeder::class);
         $this->call(ApiTableSeeder::class);
+        $this->call(SubscriptionSeeder::class);
     }
 }
