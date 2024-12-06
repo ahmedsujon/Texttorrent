52db273c8a79b1debaef95f49ad468d29070917b
nzhridoy <nayeemuzaman05@gmail.com>
2024-12-06 23:55:55 +0600

    validation export in sheets

--- app/Exports/ValidationExport.php
+++ app/Exports/ValidationExport.php
@@ -2,50 +2,31 @@
 
 namespace App\Exports;
 
-use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class ValidationExport implements FromCollection, WithHeadings
+class ValidationExport implements WithMultipleSheets
 {
-    protected $selectedItems;
+    use Exportable;
 
-    public function __construct($selectedItems)
+    protected $selectedItem;
+
+    public function __construct($selectedItem)
     {
-        $this->selectedItems = $selectedItems;
+        $this->selectedItem = $selectedItem;
     }
 
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    public function sheets(): array
     {
-        $items = DB::table('number_validations')->select('contact_lists.name as list', 'number_validations.total_credits', 'number_validations.total_number', 'number_validations.total_mobile_numbers', 'number_validations.total_landline_numbers', 'number_validations.valid_numbers', 'number_validations.invalid_numbers', 'number_validations.created_at')->join('contact_lists', 'contact_lists.id', 'number_validations.list_id')->whereIn('number_validations.id', $this->selectedItems['selectedItems'])->get();
+        // dd($this->selectedItem);
 
-        foreach ($items as $key => $item) {
-            $item->created_at = Carbon::parse($item->created_at)->isToday() ? 'Today, ' . \Carbon\Carbon::parse($item->created_at)->format('g:i A') : \Carbon\Carbon::parse($item->created_at)->format('F j, Y, g:i A');
+        $types = ['valid', 'invalid'];
+        $sheets = [];
+
+        foreach ($types as $type) {
+            $sheets[] = new ValidationSheets($this->selectedItem, $type);
         }
 
-        return $items;
-    }
-
-    /**
-     * Define the headings for the exported file.
-     *
-     * @return array
-     */
-    public function headings(): array
-    {
-        return [
-            'File Name',
-            'Validator Credits',
-            'Total Numbers',
-            'Total Mobile Numbers',
-            'Total landline Numbers',
-            'Valid Numbers',
-            'Invalid Numbers',
-            'Created At'
-        ];
+        return $sheets;
     }
 }
