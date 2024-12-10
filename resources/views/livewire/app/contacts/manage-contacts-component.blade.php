5e007273745821e75c947a327bef8c73d622a8de
nzhridoy <nayeemuzaman05@gmail.com>
2024-12-10 23:04:42 +0600

    unvalidated number

--- resources/views/livewire/app/contacts/manage-contacts-component.blade.php
+++ resources/views/livewire/app/contacts/manage-contacts-component.blade.php
@@ -421,7 +421,7 @@
                                                 </h5>
 
                                                 @if ($contact->valid == 'Invalid' || $contact->validation_process != 1)
-                                                    <span title="Invalid Number" class="ms-1">
+                                                    <span title="Unvalidated number" class="ms-1">
                                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class='text-warning' width="16" height="16" stroke-width="2"> <path d="M12 9v4"></path> <path d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z"></path> <path d="M12 16h.01"></path> </svg>
                                                     </span>
                                                 @endif
